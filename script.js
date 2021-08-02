daaae50ab927d237182190b8ad79ddc17d8e5b4c
rigole <foplacide@gmail.com>
2021-08-02 22:28:28 +0100

    Updating the DOM with the javascript

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const completeList = document.getElementById('complete-list');
 const onHoldList = document.getElementById('on-hold-list');
 
 // Items
+let updatedOnLoad = false;
 
 
 
@@ -38,8 +39,6 @@ function getSavedColumns() {
     }
 }
 
-getSavedColumns();
-updateSavedColumns();
 
 // Set localStorage Arrays
 function updateSavedColumns() {
@@ -53,9 +52,17 @@ function updateSavedColumns() {
 
 // Create DOM Elements for each list item
 function createItemEl(columnEl, column, item, index) {
+/*  console.log('columnEl:', columnEl);
+    console.log('column:', column);
+    console.log('item:', item);
+    console.log('index:', index);*/
 
+    // List Item
     const listEl = document.createElement('li');
     listEl.classList.add('drag-item');
+    listEl.textContent = item;
+    // Append
+    columnEl.appendChild(listEl);
 }
 
 
@@ -63,16 +70,34 @@ function createItemEl(columnEl, column, item, index) {
 // Update Columns in DOM - Reset HTML, Filter Array, Update localStorage
 function updateDOM() {
     // Check localStorage once
+    if (!updatedOnLoad){
+        getSavedColumns();
+    }
 
     // Backlog Column
-
+    backlogList.textContent = '';
+    backlogListArray.forEach((backlogItem, index) => {
+        createItemEl(backlogList, 0, backlogItem, index);
+    })
     // Progress Column
+    progressList.textContent = '';
+    progressListArray.forEach((progressItem, index) => {
+        createItemEl(progressList, 0, progressItem, index);
+    })
 
     // Complete Column
+    completeList.textContent = '';
+    completeListArray.forEach((completeItem, index) => {
+        createItemEl(completeList, 0, completeItem, index);
+    })
 
     // On Hold Column
+    onHoldList.textContent = '';
+    onHoldListArray.forEach((onHoldItem, index) => {
+        createItemEl(onHoldList, 0, onHoldItem, index);
+    })
 
     // Run getSavedColumns only once, Update Local Storage
-
-
 }
+// On Load
+updateDOM();
